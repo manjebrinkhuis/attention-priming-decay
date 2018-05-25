a8657017c7b0f232d40252ea274590f3716cb61b
Manje Brinkhuis <manjebrinkhuis@gmail.com>
2018-05-25 10:30:39 +0000

    update run command, pipe, hrf_estimation

--- docker/neuro-docker/run_terminal.sh
+++ docker/neuro-docker/run_terminal.sh
@@ -7,7 +7,8 @@ docker run --rm \
     -p 8888:8888 \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=unix$DISPLAY \
-    -v /mnt:/data \
+    -v /mnt/ext_data:/data/ext \
+    -v /mnt/data:/data/host \
     -v /mnt/data/Apps/Linux/freesurfer:/opt/freesurfer \
     --name neuro-docker \
     manjebrinkhuis/neuro-docker
