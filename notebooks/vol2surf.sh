c4f83dc2b4198b02783981059f407bd852be02b1
Manje Brinkhuis <manjebrinkhuis@gmail.com>
2019-08-04 00:14:25 +0200

    vol2surf cmd

---
+++ notebooks/vol2surf.sh
@@ -0,0 +1,13 @@
+mri_vol2surf \
+    --mni152reg \
+    --surf white \
+    --src clr12_overlap.nii.gz \
+    --out clr12_overlap.lh.mgz \
+    --hemi lh
+    
+mri_vol2surf \
+    --mni152reg \
+    --surf white \
+    --src clr12_overlap.nii.gz \
+    --out clr12_overlap.rh.mgz \
+    --hemi rh
